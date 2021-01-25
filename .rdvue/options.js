f344a454f4f20c228a011f8f67e6a27c520da93c
Carl Clarke <carlclarke@outlook.com>
2021-01-25 17:48:34 -0500

    RDVue 1.0 Base

---
+++ .rdvue/options.js
@@ -0,0 +1,6 @@
+module.exports = {i18n: {
+  locale: 'en',
+  fallbackLocale: 'es',
+  localeDir: 'locales',
+  enableInSFC: false
+ },};
---
+++ .rdvue/routes.js
@@ -0,0 +1,9 @@
+export default [{
+  path: '/buefy-sample',
+  name: 'buefy-sample',
+  component: require('@/pages/buefy-sample/buefy-sample.vue').default
+ },{
+  path: '/hello-i18n',
+  name: 'i18nTest',
+  component: require('@/pages/hello-i18n/hello-i18n.vue').default
+ },];

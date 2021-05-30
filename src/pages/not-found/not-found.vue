e3d4303e96dbaf18c8c95c149352cf12ab9f95d7
andre-vidal <avidal@realdecoy.com>
2021-05-30 17:00:01 +0000

    FP-431: add 404 page for router fallback - extend tailwind config to include larger font sizes

---
+++ src/pages/not-found/not-found.vue
@@ -0,0 +1,12 @@
+<style scoped lang="scss" src="./not-found.scss"></style>
+<script lang="ts" src="./not-found.ts"></script>
+<template>
+  <div class="[ not-found grid justify-items-center items-center ]">
+    <div class="[ text-center max-w-sm text-typography-light ]">
+      <p class="[ text-8xl font-thin leading-snug ]">404</p>
+      <h2 class="[ opacity-25 ]">Page Not Found</h2>
+      <p class="[ mt-5 ]">The page you are looking for doesn't exists<br>or other errors occured.
+      <span @click="$router.go(-1)" class="[ cursor-pointer underline ]">Go back</span></p>
+    </div>
+  </div>
+</template>

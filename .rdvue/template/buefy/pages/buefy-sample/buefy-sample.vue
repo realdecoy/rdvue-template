9a3f7a0068e234b2a65fa5e1549c5c2d243ba861
andre-vidal <avidal@realdecoy.com>
2021-07-13 21:09:13 +0000

    update buefy sample page imports and layout

--- .rdvue/template/buefy/pages/buefy-sample/buefy-sample.vue
+++ .rdvue/template/buefy/pages/buefy-sample/buefy-sample.vue
@@ -2,6 +2,10 @@
 import { Vue, Component } from 'vue-property-decorator';
 import { RequireVue } from '@/modules/core/constants';
 
+const { 
+  BButton
+} = require('@realdecoy/buefy/dist/components/button') as RequireVue;
+
 const {
   BDropdown,
   BDropdownItem,
@@ -10,17 +14,17 @@ const {
 // tslint:disable-next-line:no-var-requires
 const { BIcon } = require('@realdecoy/buefy/dist/components/icon') as RequireVue;
 
-@Component({ components: { BDropdown, BDropdownItem, BIcon } })
+@Component({ components: { BDropdown, BDropdownItem, BIcon, BButton } })
 export default class extends Vue {}
 </script>
 
 <template>
   <div class="[ text-center ]">
     <b-dropdown aria-role="list">
-      <button class="button" slot="trigger" slot-scope="{ active }">
+      <b-button class="button" slot="trigger" type="is-primary" slot-scope="{ active }">
         <span>Click me!</span>
         <BIcon pack="fas" :icon="active ? 'chevron-up' : 'chevron-down'" />
-      </button>
+      </b-button>
 
       <b-dropdown-item aria-role="listitem">Action</b-dropdown-item>
       <b-dropdown-item aria-role="listitem">Another action</b-dropdown-item>

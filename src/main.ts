8eacef9b2666585d15e86878b3d3564cb9ebfd38
Carl Clarke <carlclarke@outlook.com>
2023-02-28 16:57:29 -0500

    Added i18n plugin

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createFontAwesome } from '@/modules/core'
+import { createFontAwesome, createI18n} from '@/modules/core';
 import App from './components/@app'
 import router from './configs/router'
 import '@/assets/theme/@main.scss'
 
-
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
+app.use(createPinia())
+app.use(createI18n({}));
+  // legacy: false,
+  // allowComposition: true,
+  // locale: 'en',
+  // // tslint:disable-next-line: no-unsafe-any
+  // fallbackLocale: 'en',
+  // messages: {
+  //   en: {
+  //     "message": "Welcome to RDVue!"
+  //   }
+  // }
+// }));
 app.use(createFontAwesome())
 app.mount('#app')
--- src/modules/core/index.ts
+++ src/modules/core/index.ts
@@ -1,1 +1,2 @@
 export { createFontAwesome } from './font-awesome-plugin';
+export { createI18n } from './i18n-plugin';

05a4827dd623050703bd5f32b691edfcdeb384d1
Tyrone Taylor <28680107+ttaylor92@users.noreply.github.com>
2023-06-25 19:25:56 -0500

    Added localization and element plus library support

---
+++ .rdvue/template/localization/pages/hello-i18n/hello-i18n.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p class="[ text-center ]">{{ $t('message') }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Setup, Vue } from 'vue-facing-decorator';
+import { useI18n } from 'vue-i18n';
+
+@Component({})
+export default class HelloI18n extends Vue {
+
+  @Setup(() => useI18n())
+  public i18n!: ReturnType<typeof useI18n>;
+}
+</script>
--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,27 @@ import { createFontAwesome, createI18n } from '@/modules/core';
 import App from './components/@app';
 import router from './configs/router';
 import '@/assets/theme/@main.scss';
+import '@/modules/rdvue/components/styles';
+// @ts-expect-error: Incase bamboo file is empty and not recognized as a module
+import * as RdvueModules from '@/modules/rdvue';
+// @ts-expect-error: Incase bamboo file is empty and not recognized as a module
+import * as RdvueComponents from '@/modules/rdvue/components';
 
 export const app = createApp(App);
+
+// Register Rdvue Modules and Components
+for (const [key, component] of Object.entries(RdvueComponents)) {
+    if(component) {
+        app.component(key, component as any);
+    }
+}
+
+for (const [key, module] of Object.entries(RdvueModules)) {
+    if(module) {
+        app.use(module as any);
+    }
+}
+
 
 app.use(router);
 app.use(createPinia());

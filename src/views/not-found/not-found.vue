69632004c6400409390166a8a970d83fed50c4b6
Tahjyei Thompson <tahjyeithompson@gmail.com>
2023-08-21 13:35:44 -0500

    feat: Update styles and classes in not-found.vue
    - Updated the grid and alignment classes for the not-found component
    - Changed the typography class for better readability
    - Increased the height of the component to cover the whole screen

--- src/views/not-found/not-found.vue
+++ src/views/not-found/not-found.vue
@@ -1,8 +1,8 @@
 <style scoped lang="scss" src="./not-found.scss"></style>
 <script lang="ts" src="./not-found.ts"></script>
 <template>
-  <div class="[ not-found grid justify-items-center items-center ]">
-    <div class="[ text-center max-w-sm text-typography-light ]">
+  <div class="[ not-found grid justify-items-center items-center ][ h-screen ]">
+    <div class="[ text-center max-w-sm text-typography-primary ]">
       <p class="[ text-8xl font-thin leading-snug ]">404</p>
       <h2 class="[ opacity-25 ]">Page Not Found</h2>
       <p class="[ mt-5 ]">

45f277919bff067f60b1c7db9f5c4eccdc535cd9
Tahjyei Thompson <tahjyeithompson@gmail.com>
2023-08-28 16:01:50 -0500

    Delete component base and emit functions.

--- src/modules/component/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './base';
-export * from './hooks';
-export * from './prop';
-export * from './emit';
-export * from './watch';
--- src/modules/core/index.ts
+++ src/modules/core/index.ts
@@ -1,2 +1,1 @@
 export * from './font-awesome-plugin';
-export * from './i18n-plugin';
